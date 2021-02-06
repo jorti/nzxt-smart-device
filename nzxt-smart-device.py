60e77f921aea4ab29d30ad21ab8e8346eafd3e6d
Juan Orti Alcaine <jortialc@redhat.com>
2021-02-06 17:20:35 +0100

    Improve product and vendor selection.

--- nzxt-smart-device.py
+++ nzxt-smart-device.py
@@ -26,25 +26,41 @@ from time import sleep
 
 
 class NzxtDevice():
-    def __init__(self, device_vendor="0x1e71", led_color="555555", led_mode="fixed", fan_speed=100):
+    def __init__(self, device_vendor=None, device_product=None, led_color="555555", led_mode="fixed", fan_speed=100,
+                 dry_run=False):
         self.led_color = led_color
         self.led_mode = led_mode
         self.fan_speed = fan_speed
         self.device_vendor = device_vendor
+        self.device_product = device_product
+        self.dry_run = dry_run
         self._init_device()
         self.set_fan_speed(self.fan_speed)
         self.set_led(self.led_mode, self.led_color)
 
     def _init_device(self):
-        logging.info("Initializing device with vendor ID {}".format(self.device_vendor))
-        init = subprocess.run(["/usr/bin/liquidctl", "--vendor", self.device_vendor, "initialize"])
+        logging.info("Initializing device")
+        cmd = ["/usr/bin/liquidctl", ] + self._liquidctl_device_cmd() + ["initialize"]
+        if self.dry_run:
+            return
+        init = subprocess.run(cmd)
         if init.returncode != 0:
             sys.exit(init.returncode)
+
+    def _liquidctl_device_cmd(self):
+        if self.device_product:
+            return ["--product", self.device_product]
+        elif self.device_vendor:
+            return ["--vendor", self.device_vendor]
+        return None
 
     def set_fan_speed(self, percent):
         if self.fan_speed != percent:
             logging.info("Setting fan speed to {}%".format(percent))
-            result = subprocess.run(["/usr/bin/liquidctl", "--vendor", self.device_vendor, "set", "sync", "speed", str(percent)])
+            cmd = ["/usr/bin/liquidctl", ] + self._liquidctl_device_cmd() + ["set", "sync", "speed", str(percent)]
+            if self.dry_run:
+                return
+            result = subprocess.run(cmd)
             if result.returncode != 0:
                 logging.critical("Error setting fan speed")
                 sys.exit(result.returncode)
@@ -53,7 +69,10 @@ class NzxtDevice():
     def set_led(self, mode, color):
         if self.led_color != color or self.led_mode != mode:
             logging.info("Setting LED mode to {} and color {}".format(mode, color))
-            result = subprocess.run(["/usr/bin/liquidctl", "--vendor", self.device_vendor, "set", "led", "color", mode, color])
+            cmd = ["/usr/bin/liquidctl", ] + self._liquidctl_device_cmd() + ["set", "led", "color", mode, color]
+            if self.dry_run:
+                return
+            result = subprocess.run(cmd)
             if result.returncode != 0:
                 logging.critical("Error setting LEDs")
                 sys.exit(result.returncode)
@@ -78,12 +97,33 @@ def get_sensors_max_temp(devices_hint=('k10temp', 'nvme', 'coretemp', 'acpitz', 
                 continue
             for key in sensors[device][sensor].keys():
                 if "input" in key:
-                    logging.debug("Adding sensor temperature: device={} sensor={} input={}".format(device, sensor, key))
+                    logging.debug(
+                        "Adding sensor temperature: device={} sensor={} input={} value={}".format(device, sensor, key,
+                                                                                                  sensors[device][
+                                                                                                      sensor][key]))
                     temps.append(sensors[device][sensor][key])
     return max(temps)
 
 
+def calculate_fan_speed(current_max_temp, max_temp):
+    ratio = current_max_temp / args.max_temp
+    if ratio >= 0.95:
+        return 100
+    elif ratio >= 0.9:
+        return 75
+    elif ratio >= 0.85:
+        return 60
+    elif ratio >= 0.8:
+        return 40
+    elif ratio >= 0.75:
+        return 25
+    else:
+        return 0
+
+
 parser = argparse.ArgumentParser(description="NZXT smart device control")
+parser.add_argument("--product", type=str, help="Filter devices by hexadecimal product ID")
+parser.add_argument("--vendor", type=str, help="Filter devices by hexadecimal vendor ID")
 parser.add_argument("--interval", type=int, default=10, help="Interval between checks, in seconds")
 parser.add_argument("--min-speed", type=int, default=10, help="Minimum fan speed, in percent")
 parser.add_argument("--max-speed", type=int, default=100, help="Maximum fan speed, in percent")
@@ -94,6 +134,7 @@ parser.add_argument("--led-color", default="555555", help="Normal LED color")
 parser.add_argument("--led-color-warn", default="ff0000", help="Warning LED color")
 parser.add_argument("--log-level", default="INFO", help="Log level",
                     choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+parser.add_argument("--dry-run", default=False, action="store_true", help="Don't do real actions")
 args = parser.parse_args()
 
 numeric_level = getattr(logging, args.log_level.upper(), None)
@@ -103,21 +144,29 @@ logging.basicConfig(level=numeric_level)
 
 # Validate args
 if args.min_speed >= args.max_speed:
-    logging.error("Minimum fan speed has to be less than the max speed")
+    logging.critical("Minimum fan speed has to be less than the max speed")
+    sys.exit(1)
+if args.max_temp <= 0:
+    logging.critical("Max temp has to be greater than 0")
     sys.exit(1)
 
-nzxt_device = NzxtDevice(led_color=args.led_color, led_mode=args.led_mode, fan_speed=args.max_speed)
+nzxt_device = NzxtDevice(device_vendor=args.vendor, device_product=args.product, led_color=args.led_color,
+                         led_mode=args.led_mode, fan_speed=args.max_speed,
+                         dry_run=args.dry_run)
 while True:
     current_max_temp = get_sensors_max_temp()
     if current_max_temp >= args.max_temp:
-        logging.warning("Highest temperature: {}ºC".format(current_max_temp))
+        if nzxt_device.fan_speed != args.max_speed:
+            logging.warning("Temperature is greater than the configured threshold of {}ºC".format(args.max_temp))
+            logging.info("Current max temperature: {}ºC".format(current_max_temp))
         nzxt_device.set_fan_speed(args.max_speed)
         nzxt_device.set_led(args.led_mode_warn, args.led_color_warn)
     else:
-        fan_speed = (current_max_temp // args.max_temp) * 100
+        fan_speed = calculate_fan_speed(current_max_temp, args.max_temp)
         fan_speed = max(fan_speed, args.min_speed)
         fan_speed = min(fan_speed, args.max_speed)
-        logging.debug("current_max_temp: {}, fan_speed {}".format(current_max_temp, fan_speed))
+        if nzxt_device.fan_speed != fan_speed:
+            logging.info("Current max temperature: {}ºC".format(current_max_temp))
         nzxt_device.set_fan_speed(fan_speed)
         nzxt_device.set_led(args.led_mode, args.led_color)
     sleep(args.interval)
